7ff18cb2a9bc31ae124801d80d45917817d5a7b2
Tomas Cruz <tomas.r.cruz@tecnico.ulisboa.pt>
2025-02-25 12:36:53 +0000

    satellite orbiting as it should. tracers, path and preview TODO

--- main.js
+++ main.js
@@ -13,6 +13,11 @@ camera.position.set(5,3,7);
 /*********************************/
 
 
+const PLANET_MASS = 1;
+const G = 0.6;
+const PLANET_RADIUS = 2;
+
+
 /* Optional */
 const controls = new OrbitControls(camera, renderer.domElement);
 
@@ -32,41 +37,87 @@ scene.add(sun);
 
 
 /* Planet */
-const planet_geometry = new THREE.SphereGeometry(2);
+const planet_geometry = new THREE.SphereGeometry(PLANET_RADIUS);
 const planet_material = new THREE.MeshStandardMaterial({color: 0xdbbc5e});
 const planet = new THREE.Mesh(planet_geometry, planet_material);
 
 scene.add(planet);
 /**********/
 
-let sats = [];
+class Satellite extends THREE.Mesh {
+    constructor(x, y, z, vx, vy, vz, color) {
 
-function buildSat(color, pos) {
-    const sat_geometry = new THREE.SphereGeometry(0.1);
-    const sat_material = new THREE.MeshStandardMaterial({color:color, emissive:color, emissiveIntensity: 0});
-    const sat = new THREE.Mesh(sat_geometry, sat_material);
-    sat.position.copy(pos);
-    
-    scene.add(sat);
-    sats.push(sat);
+        const geometry = new THREE.SphereGeometry(0.1);
+        const material = new THREE.MeshStandardMaterial({color: color, emissive: color, emissiveIntensity: 0});
+
+        super(geometry, material);
+        this.position.set(x, y, z);
+
+        planet.add(this);
+
+        this.vx = vx;
+        this.vy = vy;
+        this.vz = vz;
+    }
+
+    update(dt) {
+
+        this.collided();
+        
+        let r = Math.sqrt(this.position.x**2 + this.position.y**2 + this.position.z**2);
+        let ag = PLANET_MASS*G/r**2;
+        let ax = -ag*this.position.x/r;
+        let ay = -ag*this.position.y/r;
+        let az = -ag*this.position.z/r;
+        this.vx += ax*dt;
+        this.vy += ay*dt;
+        this.vz += az*dt;
+        this.position.x += this.vx*dt;
+        this.position.y += this.vy*dt;
+        this.position.z += this.vz*dt;
+
+        console.log("pos = ( ", this.position.x, ", ", this.position.y, ", ", this.position.z, " )");
+        console.log("vel = ( ", this.vx, ", ", this.vy, ", ", this.vz, " )");
+    }
+    collided() {
+        if (this.geometry.parameters.radius+PLANET_RADIUS >= 
+            this.position.distanceTo(planet.position)) {
+                scene.remove(this);
+                this.geometry.dispose;
+                this.material.dispose;
+                sats.splice(sats.indexOf(this), 1);
+        }
+    }
 }
 
+let sats = [];
 
-buildSat(0x0000ff, new THREE.Vector3(0,0,3));
-buildSat(0xff0000, new THREE.Vector3(0,0,4));
-buildSat(0x00ff00, new THREE.Vector3(0,0,5));
+function buildSat(x, y, z, vx, vy, vz, color) {
+    const sat = new Satellite(x, y, z, vx, vy, vz, color);
+    
+    sats.push(sat);
+    console.log(sats);
+}
 
+buildSat(0, 0, 4, 0.5, 0.1, 0, 0xff0000);
 
 // Used for the blinking
 let clock = new THREE.Clock();
 
 function animate() {
     // Blinking
-
-        let time = clock.getElapsedTime();
-        for (let i=0; i<sats.length; i++) {
-            sats[i].material.emissiveIntensity = Math.sin(time* Math.PI);
-        }
+    console.log(sats);
+    let time = clock.getElapsedTime();
+    for (let i=0; i<sats.length; i++) {
+        sats[i].material.emissiveIntensity = Math.sin(time*1.5* Math.PI);
+        sats[i].update(0.1);
+    }
     controls.update();
     renderer.render(scene, camera);
 }
+
+const colorInput = document.getElementById("color");
+colorInput.addEventListener("input", (event) => {
+    sats[0].material.color.set(event.target.value);     // To change in a way that it changes the selected satellite
+    sats[0].material.emissive.set(event.target.value);  // To change in a way that it changes the selected satellite
+});
